13c8ee1038234b5e9f254050c89d7966e021eda8
Harshita Vyas <135962371+harshitavyas04@users.noreply.github.com>
2024-11-08 13:30:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RealNews-Vs-SatiricalNews",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here or in requirements.txt
+)
